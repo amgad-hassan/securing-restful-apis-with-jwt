ed04d89c7d071808c6c01eac0d004c4a86b9b488
Amgad Hassan <amgadhassan28@gmail.com>
2019-10-20 18:37:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+
+
+
+app.get('/api',(req,res)=>{
+    res.json({
+        message: 'welcome to the api'
+    });
+})
+
+
+
+app.post('/api/posts',verifyToken,(req,res)=>{
+    jwt.verify(req.token,'secretkey',(err,authData)=>{
+       if(err){
+           res.sendStatus(403);
+
+       }else{
+        res.json({
+            message: 'post created',
+            authData
+        });
+
+       }
+    });
+    
+})
+
+
+
+
+
+app.post('/api/login',(req,res)=>{
+    const user= {
+        id:1,
+        username:'amgad',
+        email: 'amgadhassan28@gmail.com'
+    }
+   jwt.sign({user},'secretkey',{ expiresIn: '30s' },(err,token)=>{
+       res.json({
+           token
+
+           
+        });
+   });
+});
+
+
+function verifyToken(req,res,next){
+     const bearerHeader = req.headers['authorization'];
+     if(typeof bearerHeader !== 'undefined'){
+           const bearer =bearerHeader.split(' ');
+           const bearerToken = bearer[1];
+           req.token = bearerToken;
+           next();
+     }else{
+         res.sendStatus(403);
+     }
+}
+
+const PORT = process.env.PORT||5000;
+app.listen(PORT,()=>{console.log('server running on port '+PORT);
+})
